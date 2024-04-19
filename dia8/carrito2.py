50a7ffa18fb109d34787c0579f7b2ae4116a3ca4
Michel <valerie01liz01@gmail.com>
2024-04-19 13:01:37 -0500

    cafeteria

--- dia8/carrito2.py
+++ dia8/carrito2.py
@@ -1,27 +1,29 @@
 carrito=[]
 articulos=[]
-print("TIENDA")
 
+print("TIENDA")
+#menú
 print("1. Café----1000")
 print("2. frapuchino----7000")
 print("3. Agua----500")
 
+#el usuario debe ingresar el numero del articulo que desea
 nameP=int(input("Ingrese el numero del articulo  que deseas agregar"))
 if nameP==1:
     
     cantidad=int(input("cuántos deseas comprar?"))
     
-    precioUsuario=cantidad*1000
+    precioUsuario=cantidad*1000 #dependiendo de la cantidad que el usuario compre se multiplica por el precio
    
-    print("Tu producto tiene un precio de: ",precioUsuario)
+    print("Tu producto tiene un precio de: ",precioUsuario)#mostrar al usuario el total de su compra
     
-    articulos=[nameP,precioUsuario,cantidad]
-    carrito.append(articulos)
+    articulos=[nameP,precioUsuario,cantidad]#guardar en la lista articulos los datos del usuario
+    carrito.append(articulos)#los datos del usuario almacenados anteriormente se guarda en la lista carrito
 
     for articulos in carrito: 
 
         print("\nNombre--N° en menú--Precio--Cantidad")
-        print("Café ",carrito)
+        print("Café ",carrito)#mostrar al usuario su compra
         
     print("\nGRACIAS POR SU COMPRA")
     
@@ -62,4 +64,4 @@ if nameP==3:
         
     print("\nGRACIAS POR SU COMPRA")
 
-#desarrollado por Valerie Lasso
+#desarrollado por Valerie Lasso T.I 1093296678
